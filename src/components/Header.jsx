7c7d27c882d18651348f381b2232409582aa1d26
BegimaI <babdrahmanova11@gmail.com>
2023-07-01 17:53:15 +0600

    header made

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "../styles/Header.module.css";
+import logo from '../assets/logo.svg';
+import communityPeople from '../assets/communityPeople.png'
+
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <div className={styles.headerTop}>
+                    <img src={logo} alt="logo" />
+                    <div>
+                    <button className="btn">Регистрация</button>
+                    </div>
+                </div>
+
+                <div className={styles.headerBottom}>
+                    <div className="center-container">
+                        <div>
+                            <h2>Начните свой <span className="orange-color"> профессиональный путь в IT </span>
+                                с <span className="orange-color"> Boost Up </span>  - Вашим ускорителем развития!
+                            </h2>
+                        </div>
+                        <div>
+                            <p className="fw-6">
+                                Вы окончили курсы или университет и хотите начать карьеру в IT?<br />
+                                BoostUp станет вашим надёжным партнёром на этом пути.
+                            </p>
+                        </div>
+                    </div>
+                    <img src={communityPeople} alt="logo" />
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header;
