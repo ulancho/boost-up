5971c1f8c1e881e14fb0908913c652bd887d95d4
ulancho <ulan.five@gmail.com>
2023-07-03 17:24:21 +0600

    fix: Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,50 @@
-import React from "react";
-import styles from "../styles/Header.module.css";
+import React from 'react';
+import styles from '../styles/Header.module.css';
 import logo from '../assets/logo.svg';
 import communityPeople from '../assets/communityPeople.png';
 import rocket from '../assets/rocket.png';
 
-
 const Header = () => {
     return (
-        <>
-            <header>
-                <div className={styles.headerTop}>
-                    <img src={logo} alt="logo" />
-                    <div>
+        <header>
+            <div className={styles.headerTop}>
+                <img src={logo} alt="logo" />
+                <div>
                     <button className="btn">Регистрация</button>
-                    </div>
                 </div>
+            </div>
 
-                <div className={styles.headerBottom}>
-                    <div className="center-container">
-                        <div>
-                            <img className={`${styles.stickers} ${styles.rocket}`} src={rocket} alt="rocket" />
-                            <h2>Начните свой <span className="orange-color"> профессиональный путь в IT </span>
-                                с <span className="orange-color"> Boost Up </span>  - Вашим ускорителем развития!
-                            </h2>
-                        </div>
-                        <div>
-                            <p className="fw-6">
-                                Вы окончили курсы или университет и хотите начать карьеру в IT?<br />
-                                BoostUp станет вашим надёжным партнёром на этом пути.
-                            </p>
-                        </div>
+            <div className={styles.headerBottom}>
+                <div className="center-container">
+                    <div>
+                        <img
+                            className={`${styles.stickers} ${styles.rocket}`}
+                            src={rocket}
+                            alt="rocket"
+                        />
+                        <h2>
+                            Начните свой{' '}
+                            <span className="orange-color"> профессиональный путь в IT </span>с{' '}
+                            <span className="orange-color"> Boost Up </span> - Вашим ускорителем
+                            развития!
+                        </h2>
                     </div>
-                    <img className={styles.communityPeople} src={communityPeople} alt="logo" />
+                    <div>
+                        <p className="fw-6">
+                            Вы окончили курсы или университет и хотите начать карьеру в IT?
+                            <br />
+                            BoostUp станет вашим надёжным партнёром на этом пути.
+                        </p>
+                    </div>
                 </div>
-            </header>
-        </>
-    )
-}
+                <img
+                    className={styles.communityPeople}
+                    src={communityPeople}
+                    alt="communityPeople"
+                />
+            </div>
+        </header>
+    );
+};
 
 export default Header;
