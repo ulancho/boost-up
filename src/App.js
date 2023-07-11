deb92af193fd028466394c6f9105334047133def
ulancho <ulan.five@gmail.com>
2023-07-11 16:38:38 +0600

    changed path header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
-import Header from "./components/Header";
+import Header from './components/Header/Header';
 import './styles/style.css'
 
 
