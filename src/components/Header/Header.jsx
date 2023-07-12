f8ef2339063f6c85a55010076610bc15f13bc27f
ulancho <ulan.five@gmail.com>
2023-07-12 18:03:02 +0600

    fixed header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,42 +7,34 @@ import rocket2 from '../../assets/rocket.svg';
 const Header = () => {
     return (
         <header>
-            <div className={styles.headerTop}>
+            <div className={`main-center-container ${styles.headerTop}`}>
                 <img src={logo} alt="logo" />
-                <div>
-                    <button className="btn">Регистрация</button>
-                </div>
+                <button className="btn">Регистрация</button>
             </div>
 
-            <div className={styles.headerBottom}>
-                <div className="center-container">
-                    <div>
-                        <img
-                            className={`${styles.stickers} ${styles.rocket}`}
-                            src={rocket2}
-                            alt="rocket"
-                        />
-                        <h2>
-                            Начните свой{' '}
-                            <span className="orange-color"> профессиональный путь в IT </span>с{' '}
-                            <span className="orange-color"> Boost Up </span> - Вашим ускорителем
-                            развития!
-                        </h2>
-                    </div>
-                    <div>
-                        <p className="fw-6">
-                            Вы окончили курсы или университет и хотите начать карьеру в IT?
-                            <br />
-                            BoostUp станет вашим надёжным партнёром на этом пути.
-                        </p>
-                    </div>
+            <div className={`main-center-container ${styles.headerBottom}`}>
+                <div className={styles.titleBox}>
+                    <img
+                        className={`${styles.stickers} ${styles.rocket}`}
+                        src={rocket2}
+                        alt="rocket"
+                    />
+                    <h2>
+                        Начните свой{' '}
+                        <span className="orange-color"> профессиональный путь в IT </span>с{' '}
+                        <span className="orange-color"> Boost Up </span> - Вашим ускорителем
+                        развития!
+                    </h2>
                 </div>
-                <img
-                    className={styles.communityPeople}
-                    src={communityPeople}
-                    alt="communityPeople"
-                />
+                <div className={styles.subtitleBox}>
+                    <p className="fw-6">
+                        Вы окончили курсы или университет и хотите начать карьеру в IT?
+                        <br />
+                        BoostUp станет вашим надёжным партнёром на этом пути.
+                    </p>
+                </div>
             </div>
+            <img className={styles.communityPeople} src={communityPeople} alt="communityPeople" />
         </header>
     );
 };
