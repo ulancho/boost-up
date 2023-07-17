c92031b7f8359efaa7f7827bfe7c339ff3325821
ulancho <ulan.five@gmail.com>
2023-07-17 13:54:21 +0600

    fixed AboutInternship

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import Content from './components/Content';
+import Footer from '../../components/Footer';
 
 const MainPage = () => {
     return (
--- src/pages/main/components/aboutInternship/AboutInternship.jsx
+++ src/pages/main/components/aboutInternship/AboutInternship.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import internship from '../../../../assets/internship.svg';
+import internshipDesktop from '../../../../assets/internship.svg';
+import internshipMobile from '../../../../assets/internship.png';
 import styles from './AboutInternshipt.module.css';
 
 const AboutInternship = () => {
@@ -8,7 +9,16 @@ const AboutInternship = () => {
             <h2 className="text-left orange-color"> Стажировка в BoostUp - </h2>
             <div className="center-wrapper">
                 <div className="img-container">
-                    <img src={internship} alt="стажировка" />
+                    <img
+                        className={styles.internshipDesktop}
+                        src={internshipDesktop}
+                        alt="стажировка"
+                    />
+                    <img
+                        className={styles.internshipMobile}
+                        src={internshipMobile}
+                        alt="стажировка"
+                    />
                 </div>
                 <div className="text-right">
                     <div className="wrapper">
@@ -18,7 +28,7 @@ const AboutInternship = () => {
                             тонкостях работы в IT изнутри и сделать первый важный шаг в вашей
                             карьере.
                         </p>
-                        <h2 className="fs-25 fw-5 mt-30">
+                        <h2 className={`fs-25 fw-5 ${styles.subtitle}`}>
                             Присоединяйтесь к нам и ускорьте свое развитие в IT с BoostUp!{' '}
                         </h2>
                         <div className="pt-25">
