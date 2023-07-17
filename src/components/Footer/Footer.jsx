dc06641dd8f442beb05f2a460068022198d0f9a5
ulancho <ulan.five@gmail.com>
2023-07-17 18:11:06 +0600

    fixed Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import logo from '../assets/logoWhiteText.svg';
-import instagramIcon from '../assets/instagramIcon.svg';
-import messageIcon from '../assets/messageIcon.svg';
-import whatsappIcon from '../assets/whatsappIcon.svg';
-import telegramIcon from '../assets/telegramIcon.svg';
+import styles from './Footer.module.css';
+import logo from '../../assets/logoWhiteText.svg';
+import instagramIcon from '../../assets/instagramIcon.svg';
+import messageIcon from '../../assets/messageIcon.svg';
+import whatsappIcon from '../../assets/whatsappIcon.svg';
+import telegramIcon from '../../assets/telegramIcon.svg';
 
 const Footer = () => {
     return (
-        <>
-            <footer>
-                <div>
-                    <img className="logo" src={logo} alt="logo" />
+        <footer className={`${styles.footer}`}>
+            <div className={`main-center-container ${styles.footerContainer}`}>
+                <div className={styles.logo}>
+                    <img src={logo} alt="logo" />
                 </div>
-                <div className="pl-60">
+                <div className={styles.menu}>
                     <ul>
                         <li>
                             <a className="white-font fw-7" href="#">
@@ -31,7 +32,7 @@ const Footer = () => {
                         </li>
                     </ul>
                 </div>
-                <div>
+                <div className={styles.address}>
                     <ul>
                         <li>
                             <a className="white-font fw-7" href="#">
@@ -46,31 +47,28 @@ const Footer = () => {
                         </li>
                     </ul>
                 </div>
-                <div className="pt-20 social-media-wrapper">
-                    <a
-                        href="https://www.instagram.com/booster.school_/"
-                        className="icon-link"
-                        target="_blank">
+                <div className={styles.socialMediaWrapper}>
+                    <a href="#" target="_blank">
                         <img src={instagramIcon} alt="" />
                     </a>
-                    <a href="#" className="icon-link">
+                    <a href="#" target="_blank">
                         <img src={messageIcon} alt="" />
                     </a>
-                    <a href="#" className="icon-link">
+                    <a href="#" target="_blank">
                         <img src={whatsappIcon} alt="" />
                     </a>
-                    <a href="#" className="icon-link">
+                    <a href="#" target="_blank">
                         <img src={telegramIcon} alt="" />
                     </a>
                 </div>
-                <div className="block">
+                <div className={styles.copyRight}>
                     <p className="white-font fw-7 ">
                         ©2023 Booster  UP <br />
                         IT school in Bishkek
                     </p>
                 </div>
-            </footer>
-        </>
+            </div>
+        </footer>
     );
 };
 
--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import Content from './components/Content';
-import Footer from '../../components/Footer';
+import Footer from '../../components/Footer/Footer';
 
 const MainPage = () => {
     return (
         <>
             <Header />
             <Content />
-            {/*<Footer />*/}
+            <Footer />
         </>
     );
 };
