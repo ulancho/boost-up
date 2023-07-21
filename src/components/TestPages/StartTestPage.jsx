99331e4814182ab574a20f474d8e573233f30512
BegimaI <babdrahmanova11@gmail.com>
2023-07-21 16:48:45 +0600

    have done StartTestPage

--- src/components/TestPages/StartTestPage.jsx
+++ src/components/TestPages/StartTestPage.jsx
@@ -5,12 +5,14 @@ const StartTestPage = () => {
     return (
         <div className="one-page">
             <SimpleHeader />
-            <div className="text-align-center">
-                <h2 className="fw-5 fs-25 mt-240 ">
-                    Пройдите быстрый тест 15 вопросов за 15 минут <br /> выберите один или несколько
-                    правильных ответов
-                </h2>
-                <button className="btn fw-7 mt-30">Начать</button>
+            <div className="align-to-height">
+                <div className="text-align-center">
+                    <h2 className="fw-5 fs-25 mt-240 ">
+                        Пройдите быстрый тест 15 вопросов за 15 минут <br /> выберите один или
+                        несколько правильных ответов
+                    </h2>
+                    <button className="btn fw-7 mt-30">Начать</button>
+                </div>
             </div>
         </div>
     );
