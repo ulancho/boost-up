a58115f8f1039f39baf21f506535b22b816e9bf6
ulancho <ulan.five@gmail.com>
2023-07-25 17:56:28 +0600

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './styles/style.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import MainPage from './pages/main/MainPage';
+import RegistrationPage from './pages/registration/RegistrationPage';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainPage />
+    },
+    {
+        path: 'registration',
+        element: <RegistrationPage />
+    }
+]);
 
 const App = () => {
-    return <MainPage />;
+    return <RouterProvider router={router} />;
 };
 
 export default App;
