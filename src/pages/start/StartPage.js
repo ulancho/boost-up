ded1c092914434766cd19f9c375ea4fa575aaedf
ulancho <ulan.five@gmail.com>
2023-08-08 18:57:30 +0600

    added start exam

--- src/pages/start/StartPage.js
+++ src/pages/start/StartPage.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import HeaderTest from './../../components/HeaderTest/HeaderTest';
-import styles from './Start.module.css';
+import { API } from '../../api/api';
+import { useNavigate } from 'react-router-dom';
 
 const StartTestPage = () => {
+    const navigate = useNavigate();
+
+    const startExam = async () => {
+        const student_exam_id = localStorage.getItem('student_exam_id');
+        try {
+            const { data, status } = await API.start(student_exam_id);
+            if (status === 200) {
+                navigate('/test');
+            }
+        } catch (e) {
+            console.log(e);
+        } finally {
+            console.log('finally startExam');
+        }
+    };
+
     return (
         <div className="one-page">
             <HeaderTest />
@@ -12,7 +29,9 @@ const StartTestPage = () => {
                         Пройдите быстрый тест 15 вопросов за 15 минут <br /> выберите один или
                         несколько правильных ответов
                     </h2>
-                    <button className="btn fw-7 mt-30">Начать</button>
+                    <button onClick={startExam} className="btn fw-7 mt-30">
+                        Начать
+                    </button>
                 </div>
             </div>
         </div>
