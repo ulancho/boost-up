4587bab939ac0f74d568206ae1ce915315f20bd7
ulancho <ulan.five@gmail.com>
2023-08-10 17:46:10 +0600

    added active

--- src/pages/test/TestPage.js
+++ src/pages/test/TestPage.js
@@ -3,11 +3,20 @@ import HeaderTest from '../../components/HeaderTest/HeaderTest';
 import styles from './TestPage.module.css';
 import { API } from '../../api/api';
 
-const Pagination = ({ length }) => {
+const Pagination = ({ length, questionNum }) => {
     const components = [];
 
     for (let i = 0; i < length; i++) {
-        components.push(<button className={styles.ellipse_widget}>{i + 1}</button>);
+        let activeClass = '';
+        if (questionNum === i + 1) {
+            activeClass = 'e_w_orange';
+        }
+
+        components.push(
+            <button key={i} className={`${styles.ellipse_widget} ${styles[activeClass]}`}>
+                {i + 1}
+            </button>
+        );
     }
 
     return <div className={styles.widget_wrapper}>{components}</div>;
@@ -17,6 +26,9 @@ const TestPage = () => {
     const [question, setQuestion] = useState('');
     const [options, setOptions] = useState([]);
     const [totalQuestionCount, setTotalQuestionCount] = useState(0);
+    const [questionNum, setQuestionNum] = useState(0);
+    const [answers, setAnswers] = useState([]);
+
     const getQuestion = async () => {
         const student_exam_id = localStorage.getItem('student_exam_id');
         try {
@@ -25,6 +37,7 @@ const TestPage = () => {
                 setQuestion(data.question);
                 setOptions(data.options);
                 setTotalQuestionCount(data.totalQuestionCount);
+                setQuestionNum(data.questionNum);
             }
         } catch (e) {
             console.log(e);
@@ -33,45 +46,57 @@ const TestPage = () => {
         }
     };
 
+    const onChangeAnswer = (e) => {
+        const arr = [...answers];
+
+        if (e.target.checked) {
+            arr.push(e.target.value);
+            setAnswers(arr);
+        }
+    };
+
     useEffect(() => {
         getQuestion();
     }, []);
+
+    console.log('TestPage reload');
 
     return (
         <div className="main-center-container">
             <HeaderTest />
             <div className={styles.question_container}>
                 <h2 className={`${styles.fs_14} ${styles.question} fw-5 fs-25`}>{question}</h2>
-                <form className={`${styles.test_card} ${styles.mt_20} mt-30`}>
-                    {options.map((item) => {
+                <div className={`${styles.test_card} ${styles.mt_20} mt-30`}>
+                    {options.map((item, index) => {
                         return (
-                            <div className={`${styles.option}`}>
+                            <div key={index} className={`${styles.option}`}>
                                 <input
                                     type="checkbox"
                                     id={item.option_id}
                                     value={item.option_id}
                                     name="questions"
+                                    onChange={onChangeAnswer}
                                 />
                                 <label htmlFor={item.option_id}>{item.option}</label>
                             </div>
                         );
                     })}
-                </form>
+                </div>
             </div>
 
             {/*нумерация*/}
             <div className={styles.container_for_order_reverse}>
                 <div className={styles.order2}>
-                    <Pagination length={totalQuestionCount} />
+                    <Pagination length={totalQuestionCount} questionNum={questionNum} />
                 </div>
-                <div className={styles.order1}>
-                    <div className={`${styles.center_container} mt-70 text-right mr-107`}>
-                        <button className={`${styles.btn_17_102} btn btn-transparent mr-20`}>
-                            Назад{' '}
-                        </button>
-                        <button className="btn btn-size-17-102">Далее</button>
-                    </div>
-                </div>
+                {/*<div className={styles.order1}>*/}
+                {/*    <div className={`${styles.center_container} mt-70 text-right mr-107`}>*/}
+                {/*        <button className={`${styles.btn_17_102} btn btn-transparent mr-20`}>*/}
+                {/*            Назад{' '}*/}
+                {/*        </button>*/}
+                {/*        <button className="btn btn-size-17-102">Далее</button>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
             </div>
         </div>
     );
